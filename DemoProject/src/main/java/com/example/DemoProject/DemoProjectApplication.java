b3dcdf1f85c5da9006a13e66c3861bb7a430ac02
OSAMA ELHAJ <maryoud2000@gmail.com>
2022-09-08 10:20:31 +0100

    first Commit

---
+++ DemoProject/src/main/java/com/example/DemoProject/DemoProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.DemoProject;
+
+import com.example.DemoProject.Controller.BossController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectApplication.class, args);
+	}
+}
